222002dd69b376344b72c67218e0fa6b37e734f5
OatAmOats <vicente.i.tapia@gmail.com>
2023-08-17 14:25:27 -0500

    kinda did most of what i want to do for now, in the future add a cpu and let players pick their names for pass and play and maybe add cpu options so you can play against a random box picker or a monster that will destroy you every time

--- javascript.js
+++ javascript.js
@@ -1,3 +1,36 @@
+//making boxes
+const boxFactory = () => {
+    let value = 0;
+
+    const addSymbol = (symbol) => {
+        value = symbol;
+    }
+
+    const getValue = () => {
+        return value;
+    }
+    
+
+    return {
+        addSymbol,
+        getValue,
+    }
+}
+//the gameboard
+
+const playerFactory = (name, symbol) => {
+    const getName = () =>{
+        console.log(name);
+        return name;
+    }
+    const getSymbol = () =>{
+        return symbol;
+    }
+    return{
+        getName,
+        getSymbol
+    }
+}
 function GameBoard() {
     const rows = 3;
     const columns = 3;
@@ -10,8 +43,9 @@ function GameBoard() {
         }
         board.push(row);
     }
-    const getBoard = () => board; //lets me access it later
-
+    const getBoard = () => {
+        return board;
+    }
     const play = (row, column, symbol) => {
         //im gonna need to:
         //check if the game is over,
@@ -31,38 +65,34 @@ function GameBoard() {
         console.log(`${board[1][0].getValue()}, ${board[1][1].getValue()}, ${board[1][2].getValue()}`);
         console.log(`${board[2][0].getValue()}, ${board[2][1].getValue()}, ${board[2][2].getValue()}`);
     }
-    return {getBoard, play, printBoard};
-}
-
-//making boxes
-const boxFactory = () => {
-    let value = 0;
-
-    const addSymbol = (symbol) => {
-        value = symbol;
-    }
-
-    const getValue = () => value;
-    
-
-    return {
-        addSymbol,
-        getValue,
-    }
-}
-//controlling the actual game
-function GameController(p1 = "Player One", p2 = "Player Two"){
-    const board = GameBoard();
-
-    const players = [
-        {
-            name: p1,
-            symbol: 1
-        },
-        {
-            name: p2,
-            symbol: 2
+    const clear = () => {
+        for(let i = 2; i >= 0; i--){
+            for(let j = 0; j < columns; j++){
+                board[i].pop();
+            }
+            board.pop();
         }
+        for(let i = 0; i < rows; i++){
+            row = []; //turns each row into an array
+            for(let j = 0; j < columns; j++){
+                row.push(boxFactory()); //puts a box in every spot of the array, for each row
+            }
+            board.push(row);
+        }
+    }
+    return {getBoard, play, printBoard, clear};
+}
+const p1 = playerFactory("Player One", 'X');
+const p2 = playerFactory("Player Two", "O");
+const dia = document.querySelector('.endgame');
+const left = document.querySelector('.left');
+const right = document.querySelector('.right');
+//controlling the actual game
+function GameController(p1, p2){
+    const board = GameBoard();
+    const players = [
+        p1,
+        p2
     ];
 
     let activePlayer = players[0];
@@ -72,32 +102,85 @@ function GameController(p1 = "Player One", p2 = "Player Two"){
     }
 
     const getActivePlayer = () => {
-        activePlayer;
+        return activePlayer;
     }
+
 
     const newRound = () => {
         board.printBoard();
-        console.log(`${activePlayer.name}'s turn`)
+        console.log(`${activePlayer.getName()}'s turn`)
+        if(activePlayer.getSymbol() == 'X'){
+            left.setAttribute('style', 'background-color: gold');
+            right.setAttribute('style', 'background-color: white');
+        }
+        if(activePlayer.getSymbol() == 'O'){
+            left.setAttribute('style', 'background-color: white');
+            right.setAttribute('style', 'background-color: gold');
+        }
     }
-
+    const boardShare = () =>{
+        return board;
+    }
+    const checkWin = function(){
+        gameWon = false;
+        boardArr = board.getBoard();
+        if(boardArr[0][0].getValue() == boardArr[0][1].getValue() && boardArr[0][1].getValue() == boardArr[0][2].getValue() && boardArr[0][0].getValue() != 0){
+            gameWon = true;
+            console.log('wow you won!');
+        }
+        if(boardArr[0][0].getValue() == boardArr[1][0].getValue() && boardArr[1][0].getValue() == boardArr[2][0].getValue() && boardArr[0][0].getValue() != 0){
+            gameWon = true;
+            console.log('wow you won!');
+        }
+        if(boardArr[1][0].getValue() == boardArr[1][1].getValue() && boardArr[1][1].getValue() == boardArr[1][2].getValue() && boardArr[1][0].getValue() != 0){
+            gameWon = true;
+            console.log('wow you won!');
+        }
+        if(boardArr[2][0].getValue() == boardArr[2][1].getValue() && boardArr[2][1].getValue() == boardArr[2][2].getValue() && boardArr[2][0].getValue() != 0){
+            gameWon = true;
+            console.log('wow you won!');
+        }
+        if(boardArr[0][1].getValue() == boardArr[1][1].getValue() && boardArr[1][1].getValue() == boardArr[2][1].getValue() && boardArr[0][1].getValue() != 0){
+            gameWon = true;
+            console.log('wow you won!');
+        }
+        if(boardArr[0][2].getValue() == boardArr[1][2].getValue() && boardArr[1][2].getValue() == boardArr[2][2].getValue() && boardArr[0][2].getValue() != 0){
+            gameWon = true;
+            console.log('wow you won!');
+        }
+        if(boardArr[0][0].getValue() == boardArr[1][1].getValue() && boardArr[1][1].getValue() == boardArr[2][2].getValue() && boardArr[0][0].getValue() != 0){
+            gameWon = true;
+            console.log('wow you won!');
+        }
+        if(boardArr[0][2].getValue() == boardArr[1][1].getValue() && boardArr[1][1].getValue() == boardArr[2][0].getValue() && boardArr[0][2].getValue() != 0){
+            gameWon = true;
+            console.log('wow you won!');
+        }
+        return gameWon;
+    }
     const playRound = (row, column) => {
         //place player's symbol in the box
-        roundOver = false;
-        while(roundOver == false) {
-            if((board.getBoard()[row])[column].getValue() == 0){
-                console.log(
-                    `placing ${activePlayer.symbol} in box (${row}, ${column})`
-                );
-                board.play(row, column, activePlayer.symbol);
-                roundOver = true;
+        if (board.getBoard()[row][column].getValue() == 0){
+            console.log(
+                `placing ${activePlayer.getSymbol()} in box (${row}, ${column})`
+            );
+            board.play(row, column, activePlayer.getSymbol());
+    
+            // check for winner, idk how to do that at all yet
+    
+            //switch turns if game isn't over
+            if(!checkWin()){
+                changeTurn();
+                newRound();
             }
+            if(checkWin()){
+                wl = document.querySelector('.winlose');
+                wl.textContent = `${activePlayer.getName()} (${activePlayer.getSymbol()}) wins!`
+                dia.showModal();
+            }
+            
         }
-
-        // check for winner, idk how to do that at all yet
-
-        //switch turns if game isn't over
-        changeTurn();
-        newRound();
+        
     }
 
     //initializing game
@@ -105,9 +188,77 @@ function GameController(p1 = "Player One", p2 = "Player Two"){
 
     return {
         playRound,
-        getActivePlayer
+        getActivePlayer,
+        boardShare,
+        checkWin
     };
 }
+resetbtn = document.querySelector('.reset');
+resetbtn.addEventListener('click', function(){
+    dia.close();
+    const board = game.boardShare();
+    boxes.forEach((box) => {
+        box.textContent = '';
+    })
+    board.clear();
+    
+})
+// //create the actual game
+const game = GameController(p1,p2);
+// //ok i tihnk i need to keep track of whether or not a space is taken from outside the class structure which is annoying but not too bad.
 
-//create the actual game
-const game = GameController();
+
+boxSymbol = function(box, symbol){
+    if(!game.checkWin())
+        if(box.textContent == ""){
+            const txt = document.createTextNode(symbol);
+            box.appendChild(txt);
+        }
+    
+}
+boxes = document.querySelectorAll('.box');
+clr = document.querySelector('.clrbtn');
+clr.addEventListener('click', function(){
+    const board = game.boardShare();
+    boxes.forEach((box) => {
+        box.textContent = '';
+    })
+    board.clear();
+})
+
+aa.addEventListener('click', function(){
+    boxSymbol(aa, game.getActivePlayer().getSymbol());
+    game.playRound(0,0);
+})
+ab.addEventListener('click', function(){
+    boxSymbol(ab, game.getActivePlayer().getSymbol());
+    game.playRound(0,1);
+})
+ac.addEventListener('click', function(){
+    boxSymbol(ac, game.getActivePlayer().getSymbol());
+    game.playRound(0,2);
+})
+ba.addEventListener('click', function(){
+    boxSymbol(ba, game.getActivePlayer().getSymbol());
+    game.playRound(1,0);
+})
+bb.addEventListener('click', function(){
+    boxSymbol(bb, game.getActivePlayer().getSymbol());
+    game.playRound(1,1);
+})
+bc.addEventListener('click', function(){
+    boxSymbol(bc, game.getActivePlayer().getSymbol());
+    game.playRound(1,2);
+})
+ca.addEventListener('click', function(){
+    boxSymbol(ca, game.getActivePlayer().getSymbol());
+    game.playRound(2,0);
+})
+cb.addEventListener('click', function(){
+    boxSymbol(cb, game.getActivePlayer().getSymbol());
+    game.playRound(2,1);
+})
+cc.addEventListener('click', function(){
+    boxSymbol(cc, game.getActivePlayer().getSymbol());
+    game.playRound(2,2);
+})
